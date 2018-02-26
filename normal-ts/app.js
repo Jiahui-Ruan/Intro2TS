fd492d10b5c2612ccec1cab29b1e84a6f97b0e2e
Jiahui Ruan <ryanruanwork@gmail.com>
2018-02-26 15:27:11 -0800

    learn how to use typings

--- normal-ts/app.js
+++ normal-ts/app.js
@@ -1,15 +1,4 @@
 "use strict";
-// primitive
-var myAge;
-myAge = 27;
-// array
-var hobbies = ["Cooking", "Shopping"];
-// enum
-var Color;
-(function (Color) {
-    Color[Color["Red"] = 0] = "Red";
-    Color[Color["Blue"] = 100] = "Blue";
-    Color[Color["Green"] = 101] = "Green";
-})(Color || (Color = {}));
-var color = Color.Green;
-console.log(color);
+Object.defineProperty(exports, "__esModule", { value: true });
+require("jQuery");
+$("#app").css({ "background-color": "green" });
--- normal-ts/app.ts
+++ normal-ts/app.ts
@@ -1,15 +1,2 @@
-// primitive
-let myAge: number;
-myAge = 27;
-
-// array
-let hobbies: string[] = ["Cooking", "Shopping"];
-
-// enum
-enum Color {
-    Red,
-    Blue = 100,
-    Green
-}
-let color = Color.Green;
-console.log(color);
+import "jQuery";
+$("#app").css({"background-color": "green"});
